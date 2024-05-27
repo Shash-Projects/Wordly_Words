98ccf64b2c266fa854d3ed0d40b2902b2fa4b3d3
Shashwat <shash01nautiyal@gmail.com>
2024-05-27 18:46:10 +0530

    changes made to display logo

--- lib/src/components/Navbar.jsx
+++ lib/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar
                                 {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-purple-500 rounded-full" viewBox="0 0 24 24">
                                     <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                                 </svg> */}
-                                <img className='pl-2' src="public/dic_logo.png" alt="oops" />
+                                <img className='pl-2' src="/dic_logo.png" alt="oops" />
 
                                 <span className="ml-3 text-2xl">Wordly Words</span>
                             </a>
